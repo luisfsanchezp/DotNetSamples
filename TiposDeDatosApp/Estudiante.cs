dc24ef8bf1b9eaaa494a04c1a06783f7f31e7da2
Luis Fernando Sanchez <luisfernando.sanchez@hotmail.com>
2025-11-11 17:03:52 -0500

    Se crea la clase base Persona, adiciono la clase Estudiante que herreda sus propiedades y construyo un metodo para inicializar el objeto, luego agrego el metodo de consumo para la clase Program.

--- TiposDeDatosApp/Estudiante.cs
+++ TiposDeDatosApp/Estudiante.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TiposDeDatosApp
 {
     //La clase estudiante hereda de la clase Persona.
@@ -5,10 +7,22 @@ namespace TiposDeDatosApp
     {
         //no necesito agregar ninguna propiedad porque 
         //la clase base tiene todos los campos que requiere.
-       
+        //Creo un Constructor para la clase que permit ainicializar las propiedades.       
+
+       public Estudiante(int idest, string nom, string apel, string dir, string ema, int tel, DateTime fen, DateTime fin)
+        {
+            this.Id = idest;
+            this.Nombres = nom;
+            this.Apellidos = apel;
+            this.Direccion = dir;
+            this.Email = ema;
+            this.Telefono = tel;
+            this.FeNacimiento = fen;
+            this.FeIngreso = fin;
+        }
         public void GetInfoEstudiante()
         {
-            Console.WriteLine(this.Nombre + " " + this.Apellido);
+            Console.WriteLine(this.Nombres + " " + this.Apellidos);
         }
     }
 }
--- TiposDeDatosApp/Program.cs
+++ TiposDeDatosApp/Program.cs
@@ -6,8 +6,10 @@ namespace TiposDeDatosApp
     {
         static void Main(string[] args)
         {
+            DateTime fing = DateTime.UtcNow;
+            DateTime fnac = DateTime.Parse("04/20/2009");
             //Creamos una instancia del objeto Estudiante.
-            Estudiante alumno = new Estudiante();
+            Estudiante alumno = new Estudiante(98646, "Luis Fernando", "Sanchez Perez","Calle con carera y numero","yosisoy@elmejor.com",31200000,fnac,fing);
             alumno.GetInfoEstudiante();
         }
     }
